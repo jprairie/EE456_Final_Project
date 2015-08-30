1f92a0c60bdc89a5af70d104c8c06c43a6a6ad8d
Jason Prairie <prairie.jason@gmail.com>
2015-08-30 11:53:47 -0500

    added omnigraffle drawn targets

--- MATLAB/test_bed.m
+++ MATLAB/test_bed.m
@@ -2,7 +2,7 @@ clear all;
 close all;
 home;
 
-image_name = 'Test_Scan.jpeg';
+image_name = '_sim_bullet_holes.jpg';
 %image_name = 'test1.jpg';
 target_style_num = 1;
 Target_1 = Target(image_name,target_style_num);
