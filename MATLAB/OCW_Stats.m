e7ab2e1db55c74359379aaa2084f73715c619c15
Jason Prairie <prairie.jason@gmail.com>
2015-09-08 23:53:20 -0500

    working on OCW_stats class

--- MATLAB/OCW_Stats.m
+++ MATLAB/OCW_Stats.m
@@ -36,8 +36,13 @@ classdef OCW_Stats < handle
             % calculate data
             calculate_data(obj);
             
+            % create the scanned image figure
+            create_scanned_subimages(obj);
+            
             % create the composite figure
             create_composite_fig(obj);
+            
+
             
         end
         
@@ -96,34 +101,48 @@ classdef OCW_Stats < handle
         %   
         % -----------------------------------------------------------------
         function calculate_data(obj)
-
+            
             % assign the POA
             names = obj.group_names;
             for i = 1:size(names,1)
-                % assign the POA
-                POA = obj.Group.info.(names{i}).poa_center;
-                obj.data.(names{i}).POA = POA;
-                
-                % calculate the mean POI (centroid)
+                % only do computations if there are centroids to process
                 centroids = ...
                     cat(1,obj.Group.info.(names{i}).bullet_group_props. ...
                     Centroid);
-                mean_POI = sum(centroids) ./ size(centroids,1);
-                obj.data.(names{i}).mean_POI = mean_POI;
                 
-                % calculate the mean POI to POA distance
-               x1 = POA(1);
-               y1 = POA(2);           
-               x2 = mean_POI(1);
-               y2 = mean_POI(2);
-               dx = x2 - x1; % POI is reference
-               dy = y2 - y1; % POI is reference
-               mag = sqrt(dx^2 + dy^2);
-               angle = atand(dy / dx);
-               obj.data.(names{i}).POA_to_POI_RECT = [dx dy];             
-               obj.data.(names{i}).POA_to_POI_MA = [mag angle]; 
+                if ~isempty(centroids)
+                    % flag that POI are present
+                    obj.data.(names{i}).POI_present = 1;
 
-            end  
+                    % calculate the mean POI (centroid)
+                    mean_POI = sum(centroids) ./ size(centroids,1);
+                    obj.data.(names{i}).mean_POI = mean_POI;
+                    
+                    % assign the POA
+                    POA = obj.Group.info.(names{i}).poa_center;
+                    obj.data.(names{i}).POA = POA;
+                    
+                    % calculate the mean POI to POA distance
+                    x1 = POA(1);
+                    y1 = POA(2);
+                    x2 = mean_POI(1);
+                    y2 = mean_POI(2);
+                    dx = x2 - x1; % POI is reference
+                    dy = y2 - y1; % POI is reference
+                    mag = sqrt(dx^2 + dy^2);
+                    angle = atand(dy / dx);
+                    obj.data.(names{i}).POA_to_POI_RECT = [dx dy];
+                    obj.data.(names{i}).POA_to_POI_MA = [mag angle];
+                    
+                    % calculate the delta data
+                    
+                else
+                    obj.data.(names{i}).POI_present = 0;
+                    obj.data.(names{i}).mean_POI = -1;
+                    obj.data.(names{i}).POA_to_POI_RECT = [-1 -1];
+                    obj.data.(names{i}).POA_to_POI_MA = [-1 -1];
+                end
+            end
         end
         
         
@@ -160,15 +179,19 @@ classdef OCW_Stats < handle
         function create_composite_fig(obj)
             
            % get the nominal rectangular box regions for this target
-           % style type
+           % style type and set both to be the max of the two
            nom_bounds = [obj.Group.Target.approx_rect_W ...
                obj.Group.Target.approx_rect_H];
+           nom_bounds = [max(nom_bounds) max(nom_bounds)];
+           
            
            % the center of the bounds will be the normalized POA, this is
            % aleady in inches
            center_POA = nom_bounds ./ 2;
            POA_x = center_POA(1);
            POA_y = center_POA(2);
+           
+          
 
            % previously we calculated the distance that each group centroid
            % was from the POA in both x and y coordinates, plot this out
@@ -178,24 +201,87 @@ classdef OCW_Stats < handle
            ylim([0 nom_bounds(2)]); 
            hold on;
            grid on;
+           axis('square');
            title('Composite Plot of Group Centroids');
            xlabel('Target Width, Inches');
            ylabel('Target Height, Inches');
            plot(center_POA(1),center_POA(2),'Or','MarkerFaceColor','red');
+           
+           % plot a circle of 1 inch
+           r = 0.5;
+           x = center_POA(1);
+           y = center_POA(2);
+           ang=0:0.01:2*pi;
+           xp=r*cos(ang);
+           yp=r*sin(ang);
+           plot(x+xp,y+yp,'LineStyle',':','Color',[0 0 0]);
+           
+           % plot each of the centroids
            for i = 1:length(obj.group_names)
-               x = pix2inch...
-                   (obj,obj.data.(obj.group_names{i}).POA_to_POI_RECT(1))...
-                   + POA_x;
-               y = POA_y - pix2inch...
-                   (obj,obj.data.(obj.group_names{i}).POA_to_POI_RECT(2));
-               plot(x,y,'.b');
-               text(x,y,num2str(i),'VerticalAlignment','bottom', ...
-                             'HorizontalAlignment','right');
+               present = obj.data.(obj.group_names{i}).POI_present;
+               if present
+                   x = pix2inch...
+                       (obj,obj.data.(obj.group_names{i}).POA_to_POI_RECT(1))...
+                       + POA_x;
+                   y = POA_y - pix2inch...
+                       (obj,obj.data.(obj.group_names{i}).POA_to_POI_RECT(2));
+                   plot(x,y,'Ob','MarkerFaceColor','blue');
+                   text(x,y,num2str(i),'VerticalAlignment','bottom', ...
+                       'HorizontalAlignment','right');
+               end
            end
            
            
         end
         
+        
+        % -----------------------------------------------------------------
+        % Function CREATE_SCANNED_SUBIMAGES
+        %
+        % Description:
+        %   
+        %
+        % Inputs:
+        %   
+        %
+        % Outputs:
+        %   
+        % -----------------------------------------------------------------
+        function create_scanned_subimages(obj)
+            % choose what to do based on the plot style
+            plot_style = obj.Group.Target.style_num;
+            
+            switch plot_style
+                case 1
+                    figure;
+                    for i = 1:length(obj.group_names)
+                       image = ...
+                           obj.Group.info.(obj.group_names{i}).rgb_image;
+                       subplot(3,2,i);
+                       subimage(image);
+                       title(sprintf('Group %d',i));
+                    end
+                otherwise
+                    error('Plot style not recognized');
+            end  
+        end
+        
+        
+        % -----------------------------------------------------------------
+        % Function CREATE_DELTA_PLOT
+        %
+        % Description:
+        %   
+        %
+        % Inputs:
+        %   
+        %
+        % Outputs:
+        %   
+        % -----------------------------------------------------------------
+        function create_delta_plot(obj)
+            
+        end
         
         
     end
